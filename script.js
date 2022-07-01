76fb91573924ffed0955259b6f15c3c657e87dcc
Erhan <erhankubanych@gmail.com>
2022-07-01 18:08:33 +0600

    add: project

---
+++ script.js
@@ -0,0 +1,9 @@
+const $containerBtn = document.querySelectorAll('#container button');
+const $containerImg = document.querySelectorAll('img')
+console.log($containerBtn);
+console.log($containerImg);
+for (let i = 0; i < $containerBtn.length; i++) {
+	$containerBtn[i].onclick = () => {
+		alert($containerImg[i].alt)
+	};
+}
