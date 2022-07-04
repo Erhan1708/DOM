eb980e86ce2078190b72677479121ee9b5daf387
Erhan <erhankubanych@gmail.com>
2022-07-04 09:17:06 +0600

    fead:delit id and changed JS

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const $containerBtn = document.querySelectorAll('#container button');
 const $containerImg = document.querySelectorAll('img')
 console.log($containerBtn);
 console.log($containerImg);
+
 for (let i = 0; i < $containerBtn.length; i++) {
 	$containerBtn[i].onclick = () => {
 		alert($containerImg[i].alt)
